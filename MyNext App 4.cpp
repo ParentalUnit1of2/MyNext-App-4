9f9c431568bb6298948eb83396d801e052ecff19
ParentalUnit1of2 <90790291+ParentalUnit1of2@users.noreply.github.com>
2021-12-22 21:21:33 -0600

    Add project files.

---
+++ MyNext App 4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+// This is an ASCII program to find what each item is
+
+int main()
+{
+	//What number is the letter?
+	cout << (int)'a' << endl;
+	cout << int('a') << endl;//int('a') is a casting operator
+	cout << int('A') << endl;
+
+	//What letter is in the number place?
+	cout << char(65) << endl;
+
+
+	system("pause>0");
+}
